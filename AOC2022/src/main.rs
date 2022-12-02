648a427e677e6410eb546b5880326c16a0bdf19c
Muhamed Agic <38727392+Muhamedhro@users.noreply.github.com>
2022-12-02 22:53:29 +0100

    dag 2 complete

--- AOC2022/src/main.rs
+++ AOC2022/src/main.rs
@@ -1,14 +1,23 @@
 
 pub mod day1_challenge1;
+pub mod day2_challenge1;
 
 pub use day1_challenge1::*;
+pub use day2_challenge1::*;
 
 
 
-fn main() {
-    println!("Hello, world!");
+
+
+fn main() 
+{
+    /*
     let result_day_1_challenge1 = day1_challenge1::day1_challenge1();
-    let result_day_1_challenge2 = day1_challenge1::day1_challenge2();
     println!("{}", result_day_1_challenge1);
+
+    let result_day_1_challenge2 = day1_challenge1::day1_challenge2();
     println!("{}", result_day_1_challenge2);
+    */
+    let result_day_2_challenge1 = day2_challenge1::day2_challenge1();
+    println!("{}", result_day_2_challenge1);
 }
